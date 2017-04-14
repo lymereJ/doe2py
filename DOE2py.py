edfabef089a1841801d30021460bc15f20998e51
Jeremy <jeremy.lerond@gmail.com>
2017-04-14 15:54:05 -0700

    Added new functions: extract, gui.

--- DOE2py.py
+++ DOE2py.py
@@ -2,6 +2,20 @@ import os
 import sys
 import subprocess 
 import shutil
+import pyforms
+import json
+import re
+
+from pyforms import BaseWidget
+from pyforms.Controls import ControlText
+from pyforms.Controls import ControlButton
+from pyforms.Controls import ControlFile
+from pyforms.Controls import ControlCombo
+from pyforms.Controls import ControlLabel
+from pysettings import conf;
+import settings
+conf+=settings
+
 
 def DOE2ENV(act,inpf,wthf,ver,path):
 	"""
@@ -43,14 +57,24 @@ def DOE2ENV(act,inpf,wthf,ver,path):
 	elif act.upper() == "CLEAN":
 		if os.path.exists(path+"\\DOEHRREP.lin"):
 			shutil.move(path+"\\DOEHRREP.lin",inpf+".lin")							
+			if os.path.exists(path+"\\DOEHRREP.lin"):
+				os.remove(path+"\\DOEHRREP.lin")
 		if os.path.exists(path+"\\DOEHRREP.sin"):
 			shutil.move(path+"\\DOEHRREP.sin",inpf+".sin")			
+			if os.path.exists(path+"\\DOEHRREP.sin"):
+				os.remove(path+"\\DOEHRREP.sin")			
 		if os.path.exists(path+"\\DOEREP.erp"):
 			shutil.move(path+"\\DOEREP.erp",inpf+".erp")			
+			if os.path.exists(path+"\\DOEHRREP.erp"):
+				os.remove(path+"\\DOEHRREP.erp")			
 		if os.path.exists(path+"\\DOEREP.lrp"):
 			shutil.move(path+"\\DOEREP.lrp",inpf+".lrp")						
+			if os.path.exists(path+"\\DOEHRREP.lrp"):
+				os.remove(path+"\\DOEHRREP.lrp")			
 		if os.path.exists(path+"\\DOEREP.srp"):
 			shutil.move(path+"\\DOEREP.srp",inpf+".srp")
+			if os.path.exists(path+"\\DOEHRREP.srp"):
+				os.remove(path+"\\DOEHRREP.srp")			
 			
 def DOEBDL(dir):
 	"""
@@ -64,7 +88,7 @@ def DOESIM(dir):
 	"""
 	subprocess.call(dir+"DOESIM.exe")
 	
-def RunDOE2(inpf, wthf, ver):
+def RunDOE2(inpf, wthf, ver, path):
 	"""
 	Setup, run and clean-up a DOE-2 simulation for a particular input and weather file for any version of DOE-2 available.
 	"""
@@ -72,7 +96,6 @@ def RunDOE2(inpf, wthf, ver):
 		open(inpf+".inp")
 		try: 
 			open(wthf)
-			path = os.path.dirname(os.path.realpath(__file__))
 	
 			print "===== DOE-2 Simulation ====="
 			print "Input File: "+inpf
@@ -90,7 +113,87 @@ def RunDOE2(inpf, wthf, ver):
 			print "Weather file ("+wthf+") does not exist."
 	except:
 		print "Input file ("+inpf+") does not exist."
+		
+def ImportLib(ver, lib, path):
+	"""
+	Import a user defined library.
+	"""
+	shutil.copy(lib, path+"\\"+ver+"\\USRLIB.DAT")
+	
+def ExtractReports(SIMf,*args):
+	"""
+	Extract some report out of a SIM file.
+	First argument should be the path to the sim file (include the extension).
+	Other arguments the names of the reports.
+	"""
+	SIM = open(SIMf,'r').read().split('\n')
+	OUT = open(SIMf[:-4]+"-ext_rpts.SIM",'w+')
+	InReport = False
+	for j in range(0,len(args[0])):
+		Report = args[0][j]
+		for i in range(0,len(SIM)):
+			if re.search(r'REPORT- '+str(Report)+'.*.',SIM[i]) <> None:
+				InReport = True
+			if re.search(r'REPORT- .*.',SIM[i]) <> None and re.search(r'REPORT- '+str(Report)+'.*.',SIM[i]) == None and InReport == True:
+				InReport = False
+			if InReport == True:
+				OUT.write(SIM[i]+'\n')	
+	
+class DOE2py_Utility(BaseWidget):
+	
+	def __init__(self):
+		super(DOE2py_Utility,self).__init__('DOE2py Utility')
 
+		self._Input 	= ControlFile('Input file')
+		self._Weather 	= ControlFile('Weather file')
+		self._Library 	= ControlFile('User Library')
+		self._Version	= ControlCombo('DOE-2 Version')
+		#self._Status_1	= ControlLabel('Simulation Status:')
+		#self._Status_2	= ControlLabel('-')
+		self._Run		= ControlButton('Run Simulation')
+		self._Close		= ControlButton('Close')
+
+		self._formset = ['',('  ','_Input','  '),
+		                    ('', '_Weather', '' ),
+							('', '_Library', '' ),
+							('','_Version', '' ),
+							('', '_Run', '' ),
+							('','_Close',''),''] #('','_Status_1','','_Status_2', '' )
+	
+		self._Run.value = self.__RunAction
+		self._Close.value = self.__CloseAction	
+		
+		with open('settings-gui.json') as data_file:    
+			data = json.load(data_file)
+			self._Input.value = data["lastinput"]
+			self._Weather.value = data["lastweather"]
+			self._Library.value = data["lastlibrary"]
+		for i in range(0,len(data["doe2versions"])):
+			self._Version.add_item(data["doe2versions"][i])
+		
+		
+	def __RunAction(self):
+		path = os.path.dirname(os.path.realpath(__file__))
+		if self._Library.value <> "":
+			ImportLib(self._Version.value, self._Library.value, path)
+		RunDOE2(self._Input.value[:-4], self._Weather.value, self._Version.value, path)
+		#self._Status_2.value = "Completed!"
+	
+	def __CloseAction(self):
+		with open('settings-gui.json') as data_file:    
+			data = json.load(data_file)
+			data["lastinput"] = self._Input.value
+			data["lastweather"] = self._Weather.value
+			data["lastlibrary"] = self._Library.value
+		with open('settings-gui.json','w') as data_file:			
+			json.dump(data,data_file)
+		sys.exit()	
+	
 if __name__ == '__main__':
+	path = os.path.dirname(os.path.realpath(__file__))
 	if sys.argv[1] == "RunDOE2":
-		RunDOE2(sys.argv[2], sys.argv[3], sys.argv[4])
+		RunDOE2(sys.argv[2], sys.argv[3], sys.argv[4], path)
+	elif sys.argv[1] == "Extract":
+		ExtractReports(sys.argv[2],sys.argv[3:])
+	elif sys.argv[1] == "GUI":
+		pyforms.start_app( DOE2py_Utility )
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # What is DOE2py?
-It is a set of Python scripts to faciliate the process of running building energy simulations using DOE-2. **This project is a work in progress.**
+It is a set of Python scripts that can be used to facilitate the process of running building energy simulations using DOE-2. **This project is a work in progress. Use at your own risks!**
 
 # Useful scripts:
-### RunDOE2 (Works)
+### RunDOE2
 Synthax:
 `DOE2py.py RunDOE2 path_inp_file path_weather_file DOE2version`
 
@@ -10,7 +10,18 @@ Synthax:
 * `path_weather_file`: complete path to the DOE-2 input file, include the extension.
 * `DOE2version`: name of the version of DOE-2 used for the simulation
 
+### GUI
+Synthax:
+`DOE2py.py GUI`
+
+Open a small GUI that can be used to select input and weather files as well as copy library files (`Usrlib.dat`, e.g. from eQUEST) to the folder of the version of DOE-2 selected to run the simulation. The copy is done right before the simulation.
+
+### Extract
+Synthax: `DOE2py.py Extract report_name1 report_name2 etc ...`
+
+This script extract some user specified DOE-2 report out of a .SIM file. I am using this script to extract the reports requested by GBCI for review of LEED projects. They are written in a SIM file ending in `-ext_rpts.SIM`.
+
+* `report_name1`, `report_name2`, etc. Is the name of the DOE-2 report to extract from the .SIM file.
+
 # Future updates:
-* Small GUI using PyForms
-* Select user defined library library
-* Create 'nice' output form, html?, json?, sqlite? markdown?
+* Create *nice* customizable output form using the D2Result.dll file, html?, json?, sqlite? markdown?
